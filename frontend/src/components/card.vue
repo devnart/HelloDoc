b9edd6dda6950930e5d4fc0d03eda0f6751c3d54
Hamza <hr.fb001@gmail.com>
2021-06-20 23:22:45 +0100

    front-end

---
+++ frontend/src/components/card.vue
@@ -0,0 +1,50 @@
+<template>
+<router-link to="/">
+    <div class="card">
+        <i :class="icon"></i>
+        <h2>{{ title }}</h2>
+    </div>
+    </router-link>
+</template>
+
+<script>
+
+export default ({
+    props: ['title','icon'],
+    data() {
+        return {
+            
+        }
+    },
+})
+</script>
+
+<style scoped>
+.card {
+    transition: 300ms;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    background:#f5f5f5;
+    width:350px;
+    height: 350px;
+    border-radius: 10px;
+    text-decoration: none;
+
+}
+.card:hover {
+    transform: scale(1.03);
+    background:#41b883;
+}
+.card:hover i ,.card:hover h2 {
+    color: white;
+}
+.card i {
+    font-size: 55px;
+}
+.router-link-active {
+    text-decoration: none;
+}
+
+</style>
---
+++ frontend/src/views/About.vue
@@ -0,0 +1,40 @@
+<template>
+
+  <div class="container">
+    
+  <Card  title="Make an Appoienment" icon="fa fa-plus-circle" />
+  <Card title="Check Appoientement"  icon="fas fa-search"/>
+  </div>
+</template>
+
+<script>
+
+import Card from '../components/card.vue'
+
+export default {
+  data(){
+    return {
+      name : 'This is about Page'
+    }
+  },
+  components: {
+    Card
+  }
+  
+}
+</script>
+
+<style>
+  a {
+    text-decoration: none;
+    color: inherit;
+  }
+  .container {
+    display: flex;
+    justify-content: center;
+    gap: 15px;
+    height: 100vh;
+    align-items: center;
+  }
+
+</style>
